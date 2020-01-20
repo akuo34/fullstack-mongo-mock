af4272118efc0e07c5064c5f772102e5d94babf1
Andy Ting Yu Kuo <akuo34@Andys-MacBook-Air.local>
2020-01-20 12:36:05 -0800

    set up server

--- server/controller.js
+++ server/controller.js
@@ -3,10 +3,18 @@
 var Product = require('../db/')
 
 const controller = {
-  get: (req, res) => {},
-  post: (req, res) => {},
-  put: (req, res) => {},
-  delete: (req, res) => {}
+  get: (req, res) => {
+    res.status(200).send('This is your get request, modify this file to use your router!')
+  },
+  post: (req, res) => {
+    res.status(200).send('This is your post request, modify this file to use your router!')
+  },
+  put: (req, res) => {
+    res.status(200).send('This is your put request, modify this file to use your router!')
+  },
+  delete: (req, res) => {
+    res.status(200).send('This is your delete request, modify this file to use your router!')
+  }
 }
 
 module.exports = controller
