400b2f589641f20f1b537019f109c141433e8eff
Andy Ting Yu Kuo <akuo34@Andys-MacBook-Air.local>
2020-01-20 14:38:37 -0800

    write out dbhelpers

--- db/dbhelpers.js
+++ db/dbhelpers.js
@@ -1,10 +1,10 @@
 var Product = require('./');
 // complete the dbhelpers
 var helpers = {
-  getProductsHelper: () => {},
-  postProductsHelper: () => {},
-  updateProductHelper: () => {},
-  deleteProductHelper: () => {}
+  getProductsHelper: () => Product.find(),
+  postProductsHelper: (item, min_cost, curr_bid, ends_in, image) => Product.create({item, min_cost, curr_bid, ends_in, image}),
+  updateProductHelper: (_id, item, min_cost, curr_bid, ends_in, image) => Product.findByIdAndUpdate({_id}, {item, min_cost, curr_bid, ends_in, image}),
+  deleteProductHelper: (_id) => Product.findByIdAndDelete({_id})
 };
 
 module.exports = helpers
--- db/schema.js
+++ db/schema.js
@@ -8,18 +8,4 @@ var productSchema = new mongoose.Schema ({
   image: String 
 });
  
- /*
-  ensure your schema has the following:
-    item:
-      - string,
-    min_cost:
-      - number,
-    curr_bid:
-      - number,
-    ends_in:
-      - number
-    image:
-      - string
-*/
-
 module.exports = productSchema;
